97c54d4db4c843e2b2121595c42f9d846c1a1316
Michael Sprague <mikesprague@gmail.com>
2019-10-03 08:41:06 -0400

    :rotating_light: Fix linting errors

--- KeepWarm/index.js
+++ KeepWarm/index.js
@@ -1,9 +1,8 @@
-module.exports = function(context, myTimer) {
+module.exports = function (context, myTimer) {
   const timeStamp = new Date().toISOString();
 
-  if(myTimer.isPastDue)
-  {
-      context.log('JavaScript is running late!');
+  if (myTimer.isPastDue) {
+    context.log('JavaScript is running late!');
   }
   context.log('JavaScript timer trigger function ran!', timeStamp);
 
